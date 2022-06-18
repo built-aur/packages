9d4e0c8a072cae939b5ee1d48594918a783a4e19
MedzikUser <nivua1fn@duck.com>
2022-06-18 19:42:37 +0200

    addpkg: revanced-cli

--- scripts/lib/parse-conf.sh
+++ scripts/lib/parse-conf.sh
@@ -6,7 +6,7 @@ parse-conf() {
   echo "local AUR_NAME=${AUR_NAME}"
   echo "local AUR_PKGBASE=${AUR_PKGBASE}"
   echo "local AUR_UPDATED=${AUR_UPDATED}"
-  echo "local AUR_PUSH=${AUR_UPDATED}"
+  echo "local AUR_PUSH=${AUR_PUSH}"
   echo "local GITHUB_REPO=${GITHUB_REPO}"
   echo "local GITHUB_TAG=${GITHUB_TAG}"
   echo "local NPM=${NPM}"
